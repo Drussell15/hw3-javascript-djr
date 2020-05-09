f12144cd8590f5dba97227984faf8462bbba9c89
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-05-08 19:47:32 -0700

    these now are functional
    the older code would not create a password-

--- script.js
+++ script.js
@@ -1,101 +1,97 @@
-// Assignment Code
+// arrays of lowercase, uppercase, numeric, and special characters
+var generateBtn = document.querySelector("#generate");
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var passwordChar = [];
+console.log(passwordChar)
 
-var { upperCase_char_code, lowerCase_char_code, generateBtn } = newFunction();
-const Symbol_char_codes = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58, 64)
-).concat(
-    arrayFromLowToHigh(91, 96)
-).concat(
-    arrayFromLowToHigh(124, 126)
-)
-function newFunction() {
-    var generateBtn = document.querySelector("#generate");
-    const lowerCase_char_code = arrayFromLowToHigh(97, 122);
-    const upperCase_char_code = arrayFromLowToHigh(65, 90);
-    const Number_char_code = arrayFromLowToHigh(48, 57);
-    return { upperCase_char_code, lowerCase_char_code, generateBtn };
+// getPasswordOptions prompts for password rules
+function getPasswordOptions() {
+    var length = parseInt(prompt("Enter a number between 8 and 128"));
+
+    if (isNaN(length) === true) {
+        alert("Has to be an actual number!  Try again.");
+        return;
+    }
+
+    if (isNaN(length) || length < 8 || length > 128) {
+        alert("Between 8 and 128!  Try again.");
+        return;
+    }
+    //what input is the user giving
+    var includesLC = confirm("Include Lower Case?");
+    console.log(includesLC)
+    var includesUC = confirm("Include Upper Case?");
+    var includesNC = confirm("Include Numbers?");
+    var includesSC = confirm("Include Special Characters?");
+    //what if the user violates conditions?
+    if (includesLC === false && includesUC === false && includesNC === false && includesSC === false) {
+        alert("You have to choose something!  Try again.");
+        return;
+    }
+
+    // passwordEL packages up the complete ruleset
+    var passwordEl = {
+        length: length,
+        includesLC: includesLC,
+        includesUC: includesUC,
+        includesNC: includesNC,
+        includesSC: includesSC
+    };
+    return passwordEl;
 }
 
-//user prompts returns object with user responses if user says no to everything return nothing to later send alert message to user
-function userPrompts() {
-    var passwordLength = 0;
-    do {
-        passwordLength = prompt("pick a number between 8-128");
-    } while (passwordLength < 8 || passwordLength > 128);
-    //asks user to pick between 8-128 characters
-    var numbers = "";
-    do {
-        numbers = prompt("Y or N"... do you want your password to include numbers ? "));
-        while (numbers != "Y" && numbers != "N");
-        //ask user if they want upper case
-        var passwordUpper = "";
-        do {
-            passwordUpper = prompt("Y or N... Do you want your password toinclude Uppercase?");
-        } while (passwordUpper != "Y" && passwordUpper != "N");
-        //asks user if they want lower case
-        var passwordLower = "";
-        do {
-            passwordLower = prompt("Y or N...do you want your password to include Lowercase?");
-        } while (passwordLower != "Y && passwordLower !="N");
-        //asks if user wants special characters
-        var specialChar = "";
-        do {
-            specialChar = prompt("Y or N ...Do you want your password to include specialChar?");
-        } while (specialChar != "Y" && specialChar != "N");
-        if (numbers == "N" && passwordUpper == "N" && passwordLower == "N" && specialChar == "N") {
-            return null
-        } else {
-            return {
-                passwordLength: passwordLength,
-                numbers: numbers,
-                passwordUpper: passwordUpper,
-                passwordLower: passwordLower,
-                specialChar: specialChar
-            }
-        }
-        //based on user responses this will generate password by creating an array of criteria to generate
-        function generatePassword() {
-            var passwordCriteria = userPrompts();
-            console.log(passwordCriteria)
-            if (passwordCriteria == null) {
-                return "warning you must have at least one 'Y' click generate password again";
-            } else {
-                var asciiCodes = [];
-                if (passwordCriteria.numbers == "Y") {
-                    asciiCodes = asciiCodes.concat(number_char_code);
-                }
-                if (passwordCriteria.passwordUpper == "Y") {
-                    asciiCodes = asciiCodes.concat(upperCase_char_code);
-                }
-                if (passwordCriteria.passwordLower == "Y") {
-                    asciiCodes = asciiCodes.concat(lowerCase_char_code);
-                }
-                if (passwordCriteria.specialChar == "Y") {
-                    asciiCodes = asciiCodes.concat(Symbol_char_codes);
-                }
-                const passwordCharacters = []
-                for (let i = 0; < passwordCriteria.passwordLength; i++) {
-                    const characterCode = asciiCodes[Math.floor(Math.random() * asciiCodes.length)];
-                    passwordCharacters.push(String.fromCharCode(characterCode)))]
-                }
-                return passwordCharacters.join('')
-            }
-        }
-        //write the password to the #password input or something
-        function writePassword() {
-            var password = generatePassword();
-            var passwordText = document.querySelector("#password");
-            passwordText.value = password;
-        }
+// creates a function (randomPW) from a random array based on the selected length
+function randomPW(arr) {
+    var randomPW = Math.floor(Math.random() * arr.length);
+    var randomPWthePW = arr[randomPW];
+    return randomPWthePW;
+}
 
+// function generates password with user input and using randomPW
+function generatePassword() {
+    var options = getPasswordOptions();
+    var thePW = [];
+    console.log(thePW)
+    var anyChar = [];
+    var theChar = [];
+    //includeslower case letters;
+    if (options.includesLC) {
+        anyChar = anyChar.concat(lowerCaseChar);
+        theChar.push(randomPW(lowerCaseChar));
+    }
+    //include upper case letters;
+    if (options.includesUC) {
+        anyChar = anyChar.concat(upperCaseChar);
+        theChar.push(randomPW(upperCaseChar));
+    }
+    //doe it include numbers;
+    if (options.includesNC) {
+        anyChar = anyChar.concat(numericChar);
+        theChar.push(randomPW(numericChar));
+    }
+    //does it include special characters;
+    if (options.includesSC) {
+        anyChar = anyChar.concat(specialChar);
+        theChar.push(randomPW(specialChar));
+    }
 
-        // helpfunction to create arrays for ascii character groups
-        function arrayFromLowToHigh(low, high) {
-            const array = [];
-            for (let i = low; i <= high; i++) {
-                array.push(i);
-            }
-            return array;
-        }
-        // Add event listener to generate button
-        generateBtn.addEventListener("click", writePassword);
+    for (var i = 0; i < options.length; i++) {
+        var allChar = randomPW(anyChar);
+        thePW.push(allChar);
+    }
+    return thePW.join("");
+}
+//this generates the button
+var generateBtn = document.querySelector("#generate");
+//creates the password shown in textbox
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    //console.log(1);
+    passwordText.value = password;
+}
+//add event listener to generate button;
+generateBtn.addEventListener("click", writePassword);
